469e3fcf1346dd8168adcb9d5da4a9fd62645bf3
Igaguri <igagurimk@gmail.com>
2018-03-18 23:32:57 +0900

    LoadGameに追加

--- event/types/loadGame.go
+++ event/types/loadGame.go
@@ -12,20 +12,22 @@ func init() {
 }
 
 type LoadGame struct {
-	Commander    string    `json:"Commander"`
-	Credits      int64     `json:"Credits"`
-	FuelCapacity float64   `json:"FuelCapacity"`
-	FuelLevel    float64   `json:"FuelLevel"`
-	GameMode     string    `json:"GameMode"`
-	Group        string    `json:"Group"`
-	Loan         int64     `json:"Loan"`
-	Ship         string    `json:"Ship"`
-	ShipID       int64     `json:"ShipID"`
-	ShipIdent    string    `json:"ShipIdent"`
-	ShipName     string    `json:"ShipName"`
-	StartLanded  bool      `json:"StartLanded"`
-	Event        string    `json:"event"`
-	Timestamp    time.Time `json:"timestamp"`
+	Commander      string    `json:"Commander"`
+	Credits        int64     `json:"Credits"`
+	FuelCapacity   float64   `json:"FuelCapacity"`
+	FuelLevel      float64   `json:"FuelLevel"`
+	GameMode       string    `json:"GameMode"`
+	Group          string    `json:"Group"`
+	Horizons       bool      `json:"Horizons"`
+	Loan           int64     `json:"Loan"`
+	Ship           string    `json:"Ship"`
+	ShipID         int64     `json:"ShipID"`
+	ShipIdent      string    `json:"ShipIdent"`
+	ShipName       string    `json:"ShipName"`
+	Ship_Localised string    `json:"Ship_Localised"`
+	StartLanded    bool      `json:"StartLanded"`
+	Event          string    `json:"event"`
+	Timestamp      time.Time `json:"timestamp"`
 }
 
 func (e LoadGame) GetEvent() string {
